0a4ad483ce5e1cd4961787ecee070f5e71b7d7f8
aiiro <aiiro@users.noreply.github.com>
2017-07-22 17:01:13 +0900

    Add ArrayBox class, and delete Helper.php

--- src/ArrayBox/ArrayBox.php
+++ src/ArrayBox/ArrayBox.php
@@ -3,31 +3,71 @@
 namespace ArrayBox;
 
 /**
- * Class Helper
+ * Class ArrayBox
  *
  * @package ArrayBox
  */
-class Helper
+class ArrayBox
 {
+    /** @var array */
+    protected $value = [];
+
+    /**
+     * ArrayBox constructor.
+     *
+     * @param array ...$values
+     */
+    public function __construct(...$values)
+    {
+        if (count($values) === 0) {
+            return;
+        }
+
+        if (count($values) > 1) {
+            foreach ($values as $value) {
+                $this->value[] = $value;
+            }
+        }
+
+        if (is_array($values[0])) {
+            $this->value = $values[0];
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return ArrayBox
+     */
+    public function copy()
+    {
+        return clone $this;
+    }
+
     /**
      * Sort two dimensional array using the given $first, $second parameter.
      *
-     * @param array $two_dimensional_array
      * @param $first
      * @param $first_order
      * @param $second
      * @param $second_order
      * @return array
      */
-    public static function sort2Dimensional($two_dimensional_array, $first, $first_order, $second, $second_order)
+    public function sort2Dimensional($first, $first_order, $second, $second_order)
     {
-        $copy = $two_dimensional_array;
+        $copy = $this->copy()->value;
 
         $first_column = [];
         $second_column = [];
 
         // Change the array of rows to array of columns.
-        foreach ($two_dimensional_array as $key => $row) {
+        foreach ($this->value as $key => $row) {
             $first_column[$key] = $row[$first];
             $second_column[$key] = $row[$second];
         }
@@ -39,12 +79,11 @@ class Helper
     /**
      * Find duplications in multi dimensional array.
      *
-     * @param array $multi_dimensional_array Multi dimensional array
      * @return array $duplicate_values Duplicate arrays
      */
-    public static function duplicatesInMultiDimensional($multi_dimensional_array)
+    public function duplicatesInMultiDimensional()
     {
-        $serialized = array_map('serialize', $multi_dimensional_array);
+        $serialized = array_map('serialize', $this->value);
         $values_counts = array_count_values($serialized);
 
         $duplicate_values = [];
@@ -60,39 +99,37 @@ class Helper
     /**
      * Retrieve the values within the given range.
      *
-     * @param $data array
      * @param null|int $from
      * @param null|int $to
      * @return array
      */
-    public static function between($data, $from=null, $to=null)
+    public function between($from=null, $to=null)
     {
         if (empty($from) && empty($to)) {
-            return $data;
+            return $this->value;
         }
 
         if (empty($from)) {
-            return array_slice($data, $from, $to + 1);
+            return array_slice($this->value, $from, $to + 1);
         }
 
         if ($to < 0) {
-            return array_slice($data, $from, count($data));
+            return array_slice($this->value, $from, count($this->value));
         }
 
-        return array_slice($data, $from, $to);
+        return array_slice($this->value, $from, $to);
     }
 
     /**
      * Retrieve the values except for the given value.
      *
-     * @param array $data
      * @param $null
      * @param bool $preserve_key
      * @return array
      */
-    public static function except($data, $null, $preserve_key=false)
+    public function except($null, $preserve_key=false)
     {
-        $filtered = array_filter($data, function ($element) use ($null) {
+        $filtered = array_filter($this->value, function ($element) use ($null) {
             return ($element !== $null) ? true : false;
         });
 
